636ed6090b9a3babc18d7e83e473166382e7fda1
fibini <fabienswift@gmail.com>
2023-02-07 19:47:08 -0400

    Create obstacle number 2

--- src/Level.jsx
+++ src/Level.jsx
@@ -13,7 +13,7 @@ const obstacleMaterial = new THREE.MeshStandardMaterial({color: "orangered" })
 const wallMaterial = new THREE.MeshStandardMaterial({color: "slategrey" })
 /**
  * Starting Block
- */
+*/
 
 function BlockStart({position = [ 0, 0, 0] })
 {
@@ -32,7 +32,7 @@ function BlockStart({position = [ 0, 0, 0] })
 
 /**
  * Block 2
- */
+*/
 
 function BlockSpinner({position = [ 0, 0, 0] })
 {
@@ -78,11 +78,60 @@ function BlockSpinner({position = [ 0, 0, 0] })
     </group>
 }
 
+/**
+ * Block 3
+*/
+
+function BlockLimbo({position = [ 0, 0, 0] })
+{
+    const obstacle = useRef()
+    const [ timeOffset ] = useState(() => Math.random() * Math.PI * 2)
+
+    useFrame((state) =>
+    {
+        const time = state.clock.getElapsedTime()
+
+        const y = Math.sin(time + timeOffset) + 1.15
+        obstacle.current.setNextKinematicTranslation({ x: position[0], y: position[1] + y, z: position[2] })
+    })
+    
+
+    return <group position={ position }>
+
+        {/* floor 2 */}
+        <mesh
+            geometry={ boxGeometry}
+            material={ floor2Material }
+            position={ [ 0, -0.1, 0] }
+            scale={ [ 4, 0.2, 4 ] }
+            receiveShadow
+        />
+
+        {/* first obstacle */}
+        <RigidBody
+            ref={ obstacle } 
+            type="kinematicPosition" 
+            position={ [0, 0.3, 0]} 
+            restitution={ 0.2 } 
+            friction={ 0 }
+         >
+            <mesh 
+                geometry={ boxGeometry}
+                material={ obstacleMaterial }
+                scale={ [ 3.5, 0.3, 0.3 ] }
+                castShadow
+            />
+        </RigidBody>
+
+    </group>
+}
+
 export default function Level()
 {
     return <>
 
-        <BlockStart position={ [0, 0, 4 ] } />
-        <BlockSpinner position={ [0, 0, 0] } />
+        <BlockStart position={ [0, 0, 8 ] } />
+        <BlockSpinner position={ [0, 0, 4] } />
+        <BlockLimbo position={ [0, 0, 0] } />
     </>
 }
