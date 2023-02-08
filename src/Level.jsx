29fab843d86a19cb4bf8b0ebb8c32872919ff090
fibini <fabienswift@gmail.com>
2023-02-07 20:05:41 -0400

    Create obstacle number 3 and floor for end of level

--- src/Level.jsx
+++ src/Level.jsx
@@ -98,7 +98,7 @@ function BlockLimbo({position = [ 0, 0, 0] })
 
     return <group position={ position }>
 
-        {/* floor 2 */}
+        {/* floor 3 */}
         <mesh
             geometry={ boxGeometry}
             material={ floor2Material }
@@ -126,12 +126,81 @@ function BlockLimbo({position = [ 0, 0, 0] })
     </group>
 }
 
+/**
+ * Block 4
+*/
+
+function BlockAxe({position = [ 0, 0, 0] })
+{
+    const obstacle = useRef()
+    const [ timeOffset ] = useState(() => Math.random() * Math.PI * 2)
+
+    useFrame((state) =>
+    {
+        const time = state.clock.getElapsedTime()
+
+        const x = Math.sin(time + timeOffset) * 1.20
+        obstacle.current.setNextKinematicTranslation({ x: position[0] + x, y: position[1] + 0.75, z: position[2] })
+    })
+    
+
+    return <group position={ position }>
+
+        {/* floor 4 */}
+        <mesh
+            geometry={ boxGeometry}
+            material={ floor2Material }
+            position={ [ 0, -0.1, 0] }
+            scale={ [ 4, 0.2, 4 ] }
+            receiveShadow
+        />
+
+        {/* first obstacle */}
+        <RigidBody
+            ref={ obstacle } 
+            type="kinematicPosition" 
+            position={ [0, 0.3, 0]} 
+            restitution={ 0.2 } 
+            friction={ 0 }
+         >
+            <mesh 
+                geometry={ boxGeometry}
+                material={ obstacleMaterial }
+                scale={ [ 1.5, 1.5, 0.3 ] }
+                castShadow
+            />
+        </RigidBody>
+
+    </group>
+}
+
+/**
+ * Ending Block
+*/
+
+function BlockEnd({position = [ 0, 0, 0] })
+{
+    return <group position={ position }>
+
+        {/* starting floor */}
+        <mesh
+            geometry={ boxGeometry}
+            material={ floor1Material }
+            position={ [ 0, -0.1, 0] }
+            scale={ [ 4, 0.2, 4 ] }
+            receiveShadow
+        />
+    </group>
+}
+
 export default function Level()
 {
     return <>
 
-        <BlockStart position={ [0, 0, 8 ] } />
-        <BlockSpinner position={ [0, 0, 4] } />
-        <BlockLimbo position={ [0, 0, 0] } />
+        <BlockStart position={ [0, 0, 16 ] } />
+        <BlockSpinner position={ [0, 0, 12] } />
+        <BlockLimbo position={ [0, 0, 8] } />
+        <BlockAxe position={ [0, 0, 4] } />
+        <BlockEnd position={ [0, 0, 0] } />
     </>
 }
