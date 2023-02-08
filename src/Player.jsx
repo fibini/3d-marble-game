e4c2ed51707b146747a13ca009a940ba37f0e6fa
fibini <fabienswift@gmail.com>
2023-02-08 11:25:46 -0400

    Add physics to the ball

--- src/Player.jsx
+++ src/Player.jsx
@@ -1,9 +1,18 @@
+import { RigidBody } from "@react-three/rapier"
+
 export default function Player()
 {
     return <>
-    <mesh>
-        <icosahedronGeometry args={ [0.3, 1] } />
-        <meshStandardMaterial flatShading color="darkblue" />
-    </mesh>
+    <RigidBody 
+        position={ [ 0, 1, 0 ] } 
+        colliders="ball"
+        restitution={ 0.2 }
+        friction={ 1 }
+    >
+        <mesh castShadow>
+            <icosahedronGeometry args={ [0.3, 1] } />
+            <meshStandardMaterial flatShading color="darkblue" />
+        </mesh>
+    </RigidBody>
     </>
 }
