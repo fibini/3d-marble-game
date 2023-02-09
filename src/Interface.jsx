49c35a6f0b88ff25dbcb64a4e64852f4837165b2
fibini <fabienswift@gmail.com>
2023-02-09 18:16:34 -0400

    Add timer and restart html over canvas

---
+++ src/Interface.jsx
@@ -0,0 +1,10 @@
+export default function Interface()
+{
+    return < div className="interface">
+        {/* Time */}
+        <div className="time">0.00</div>
+
+        {/* Restart */}
+        <div className="restart">RESTART</div>
+    </div>
+}
