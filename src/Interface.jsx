7b6cdfcea12c2d0d2df9730f4df81663db901b08
fibini <fabienswift@gmail.com>
2023-02-10 15:34:26 -0400

    Make Restart button work and show it only when player has finished the race

--- src/Interface.jsx
+++ src/Interface.jsx
@@ -1,7 +1,11 @@
 import { useKeyboardControls } from "@react-three/drei"
+import useGame from "./stores/useGame.jsx"
 
 export default function Interface()
 {
+    const restart = useGame((state) => state.restart)
+    const phase = useGame((state) => state.phase)
+
     const forward = useKeyboardControls((state) => state.forward)
     const backward = useKeyboardControls((state) => state.backward)
     const leftward = useKeyboardControls((state) => state.leftward)
@@ -13,7 +17,7 @@ export default function Interface()
         <div className="time">0.00</div>
 
         {/* Restart */}
-        <div className="restart">RESTART</div>
+        { phase === 'ended' ? <div className="restart" onClick={ restart }>RESTART</div> : null }
 
         {/* Controls */}
         <div className="controls">
