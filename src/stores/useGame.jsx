3591ed495d440259f387139cfb961fb253c2844b
fibini <fabienswift@gmail.com>
2023-02-14 14:24:55 -0400

    increase length of race

--- src/stores/useGame.jsx
+++ src/stores/useGame.jsx
@@ -4,7 +4,7 @@ import { subscribeWithSelector } from 'zustand/middleware'
 export default create(subscribeWithSelector((set) =>
 {
     return{
-        blocksCount: 15,
+        blocksCount: 50,
         blocksSeed: 0,
 
          /**
